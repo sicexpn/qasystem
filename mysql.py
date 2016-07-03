cc89bc351c102d1bb428d4429d0db6fa8cf36cca
xingpanning <xingpanning@meituan.com>
2016-07-03 13:20:01 +0800

    样式增加，template继承

--- mysql.py
+++ mysql.py
@@ -12,20 +12,33 @@ def getConn():
 def insert(question, answer):
     db = getConn();
     ctime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    sql = "insert into search_log(question_text, answer_text, count, cts, uts) \
+    sql = "insert into search_log(question_text, answer_text, cts, uts) \
     VALUES ('%s','%s','%d','%s','%s')" % \
-          (question, answer, 1, ctime, ctime)
+          (question, answer, ctime, ctime)
 
     print "sql is " + sql
     cursor = db.cursor()
     try:
         cursor.execute(sql)
         db.commit()
+        print "insert ok"
     except:
         db.rollback()
         print "insert failed"
     db.close()
 
+
+def select(question):
+    db = getConn();
+    sql = "select id,question_text,answer_text,cts,uts from search_log where question = '" + question + "'";
+    cursor = db.cursor()
+    results = cursor.execute(sql)
+    for row in results:
+        id = row[0]
+        question_text = row[1]
+        answer_text = row[2]
+        cts = row[3]
+        uts = row[4]
 
 def insertTest():
     insert("question1", "answer1")
--- static/js/my.js
+++ static/js/my.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+	$("#p1_childs").hide();
+	$("#p2_childs").hide();
     $("#p1").click(function () {
         $("#p1_childs").toggle();
     });
