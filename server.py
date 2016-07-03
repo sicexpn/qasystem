ce2dd1f73cd97a30d94c8fe5021acf6680842d30
xingpanning <xingpanning@meituan.com>
2016-07-03 17:02:52 +0800

    答案页面完成

--- server.py
+++ server.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask import render_template,request
+from flask import render_template, request
 import os
 import sys
 
-reload(sys)  
-sys.setdefaultencoding('utf8') 
+reload(sys)
+sys.setdefaultencoding('utf8')
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/submit',methods=['POST'])
+
+
+@app.route('/submit', methods=['POST'])
 def submitQuestion():
-    if request.method=='POST':
-	question=request.form['question']
-	
-	q_file=open('./qa/questions_for_test.txt','w')
-	print >>q_file,question
-	q_file.close()
-	out=os.popen('./getAnswer.sh')
-	answers=out.read()
-	print "answer.."
-	print answers
-	answerlist=answers.strip().split('\n')
-	return render_template('index.html',question=question,answers=answerlist)
+    print request.method
+    if request.method == 'POST':
+        question = request.form['question']
+
+        print question
+        q_file = open('./qa/questions_for_test.txt', 'w')
+        print >> q_file, question
+        q_file.close()
+        out = os.popen('./getAnswer.sh')
+        answers = out.read()
+        print "answer.."
+        print answers
+        answerlist = answers.strip().split('\n')
+        return render_template('answer_page.html', question=question, answers=answerlist)
+
+
 if __name__ == '__main__':
     app.run(
-	host="0.0.0.0",
-	port=8888,
-	debug=True)
+        host="0.0.0.0",
+        port=8888,
+        debug=True)
